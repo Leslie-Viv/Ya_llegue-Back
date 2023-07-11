b9f983d3542da1ab4c7b3b2fc648a4a8a02b7d86
Andreamh30 <amh20140407@gmail.com>
2023-07-11 08:45:10 -0600

    Entity encargados y validaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EncargadosModule } from './encargados/encargados.module';
 
 
 @Module({
-  imports: [],
+  imports: [EncargadosModule],
   controllers: [AppController],
   providers: [AppService],
 })
