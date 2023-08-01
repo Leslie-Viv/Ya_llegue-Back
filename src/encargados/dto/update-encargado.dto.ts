a58f4df888b2601c803de9bd27bdcd129bcb46e5
Andreamh30 <amh20140407@gmail.com>
2023-08-01 08:18:54 -0600

    update dto encargado

--- src/encargados/dto/update-encargado.dto.ts
+++ src/encargados/dto/update-encargado.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEncargadoDto } from './create-encargado.dto';
+import { IsString } from 'class-validator';
 
-export class UpdateEncargadoDto extends PartialType(CreateEncargadoDto) {}
+export class UpdateEncargadoDto extends PartialType(CreateEncargadoDto) {
+
+    @IsString()
+    nombre: string
+    @IsString()
+    apellidos:string
+    @IsString()
+    foto:string
+}
